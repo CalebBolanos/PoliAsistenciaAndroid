1a3053afaaebbbae21e2763e034c14600fd7f601
CalebBolanos <bolanos.c@hotmail.com>
2018-05-09 22:42:20 -0500

    estadisticas para profesor completo uwu

--- app/src/main/java/edu/cecyt9/ipn/poliasistenciaandroid/profesor/AsistenciaIndividualProfesor.java
+++ app/src/main/java/edu/cecyt9/ipn/poliasistenciaandroid/profesor/AsistenciaIndividualProfesor.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
@@ -25,22 +28,37 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import edu.cecyt9.ipn.poliasistenciaandroid.R;
 import edu.cecyt9.ipn.poliasistenciaandroid.AsistenciaPorMes;
+import edu.cecyt9.ipn.poliasistenciaandroid.Sesion;
+
+import static edu.cecyt9.ipn.poliasistenciaandroid.InicioSesion.IP;
+import static edu.cecyt9.ipn.poliasistenciaandroid.InicioSesion.PUERTO;
 
 public class AsistenciaIndividualProfesor extends AppCompatActivity {
 
     ListView listaMeses;
     LineChart graficaGeneral;
+    String resultado;
+    Sesion sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asistencia_individual_profesor);
 
+        sesion = new Sesion(this);
         Toolbar toolbar = findViewById(R.id.toolbar_asistencia_individual_profesor);
         toolbar.setTitleTextColor((Color.parseColor("#ffffff")));
         setSupportActionBar(toolbar);
@@ -54,69 +72,9 @@ public class AsistenciaIndividualProfesor extends AppCompatActivity {
         }
         listaMeses = findViewById(R.id.listview_meses_profesor);
         graficaGeneral = findViewById(R.id.grafica_linechart_asistencia_individual_profesor);
-        ArrayList<String> arrayMeses = new ArrayList<>();
-        for(int i = 1; i<=12; i++){
-            arrayMeses.add("Mes " + i);
-        }
-        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayMeses);
-        listaMeses.setAdapter(adaptador);
-        listaMeses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String seleccionado = listaMeses.getItemAtPosition(i).toString();
-                switch (seleccionado){
-                    case "":
 
-                        break;
-                    default:
-                        Intent graficas = new Intent(AsistenciaIndividualProfesor.this, AsistenciaPorMes.class);
-                        startActivity(graficas);
-                        break;
-                }
-            }
-        });
-        setListViewHeightBasedOnChildren(listaMeses);
-        listaMeses.setFocusable(false);
-        generarGrafica();
+        new graficaGeneralAndroid().execute();
 
-    }
-
-    public void generarGrafica(){
-        final HashMap<Integer, String> meses = new HashMap<>();
-        meses.put(0, "Meses");
-        meses.put(1, "Enero");
-        meses.put(2, "Febrero");
-        meses.put(3, "Marzo");
-
-        ArrayList<Entry> dias = new ArrayList<>();
-        dias.add(new Entry(0f, 0f));
-        dias.add(new Entry(1f, 0f));
-        dias.add(new Entry(2f, 1f));
-        dias.add(new Entry(3f, 0f));
-
-        LineDataSet datos = new LineDataSet(dias, "Dias");
-        datos.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        datos.setFillColor(ContextCompat.getColor(this, R.color.azul));
-        datos.setDrawFilled(true);
-        datos.setLineWidth(3f);
-        datos.setColor(ContextCompat.getColor(this, R.color.azul));
-        datos.setCircleColor(ContextCompat.getColor(this, R.color.azul));
-        datos.setCircleRadius(5f);
-        LineData todo = new LineData(datos);
-
-        XAxis valoresx = graficaGeneral.getXAxis();
-        valoresx.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return meses.get((int)value);
-            }
-        });
-        valoresx.setLabelCount(meses.size(),true);
-
-        graficaGeneral.setData(todo);
-        graficaGeneral.animateY(1500, Easing.EasingOption.EaseInOutExpo);
-        graficaGeneral.setTouchEnabled(false);
-        graficaGeneral.getDescription().setText("");
     }
 
     public static void setListViewHeightBasedOnChildren(ListView listView) {
@@ -146,5 +104,172 @@ public class AsistenciaIndividualProfesor extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class graficaGeneralAndroid extends AsyncTask<String, String, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            String NAMESPACE = "http://servicios/";
+            String URL = "http://"+IP+":"+PUERTO+"/serviciosWebPoliAsistencia/alumno?WSDL";
+            String METHOD_NAME = "graficaGeneralAndroid";
+            String SOAP_ACTION = "http://servicios/graficaGeneralAndroid";
+
+
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+            request.addProperty("idPer", sesion.getIdPer());
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(request);
+
+            envelope.dotNet = false;
+
+            HttpTransportSE ht = new HttpTransportSE(URL);
+            ht.debug = true;
+
+            try{
+                ht.call(SOAP_ACTION, envelope);
+                SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
+                resultado = response.toString();
+                Log.i("Respuesta" ,  resultado);
+            }
+            catch(Exception error){
+                error.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if(success){
+                int mesActual = 0;
+                int ciclo = 0;
+                String infoMes;
+                final HashMap<Integer, String> meses = new HashMap<>();
+                ArrayList<Entry> dias = new ArrayList<>();
+                ArrayList<String> arrayMeses = new ArrayList<>();
+
+                try{
+                    JSONObject info = new JSONObject(resultado);
+                    mesActual = Integer.parseInt(info.getString("mes actual"));
+                    ciclo = Integer.parseInt(info.getString("Ciclo"));
+
+                    meses.put(0, "Meses");
+                    dias.add(new Entry(0f, 0f));
+                    if(ciclo == 1){
+                        int x = 1;
+                        for (int i = 7; i <=mesActual; i++) {
+                            infoMes = "mes " + i;
+                            meses.put(x, nombreMes(i));
+                            arrayMeses.add(nombreMes(i));
+                            dias.add(new Entry(x, Integer.parseInt(info.getString(infoMes))));
+                            x++;
+                        }
+                    }
+                    else{
+                        for (int i = 1; i <=mesActual; i++) {
+                            infoMes = "mes " + i;
+                            meses.put(i, nombreMes(i));
+                            arrayMeses.add(nombreMes(i));
+                            dias.add(new Entry(i, Integer.parseInt(info.getString(infoMes))));
+                        }
+                    }
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                ArrayAdapter adaptador = new ArrayAdapter(AsistenciaIndividualProfesor.this, android.R.layout.simple_list_item_1, arrayMeses);
+                listaMeses.setAdapter(adaptador);
+                listaMeses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String seleccionado = listaMeses.getItemAtPosition(i).toString();
+                        switch (seleccionado){
+                            case "":
+
+                                break;
+                            default:
+                                Intent graficas = new Intent(AsistenciaIndividualProfesor.this, AsistenciaPorMes.class);
+                                graficas.putExtra("mes", seleccionado);
+                                graficas.putExtra("id", sesion.getIdPer());
+                                startActivity(graficas);
+                                break;
+                        }
+                    }
+                });
+                setListViewHeightBasedOnChildren(listaMeses);
+                adaptador.notifyDataSetChanged();
+                listaMeses.setFocusable(false);
+
+                LineDataSet datos = new LineDataSet(dias, "Dias");
+                datos.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+                datos.setFillColor(ContextCompat.getColor(AsistenciaIndividualProfesor.this, R.color.azul));
+                datos.setDrawFilled(true);
+                datos.setLineWidth(3f);
+                datos.setColor(ContextCompat.getColor(AsistenciaIndividualProfesor.this, R.color.azul));
+                datos.setCircleColor(ContextCompat.getColor(AsistenciaIndividualProfesor.this, R.color.azul));
+                datos.setCircleRadius(5f);
+                LineData todo = new LineData(datos);
+
+                XAxis valoresx = graficaGeneral.getXAxis();
+                valoresx.setValueFormatter(new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+                        return meses.get((int)value);
+                    }
+                });
+                valoresx.setLabelCount(meses.size(),true);
+
+                graficaGeneral.setData(todo);
+                graficaGeneral.animateY(1500, Easing.EasingOption.EaseInOutExpo);
+                graficaGeneral.setTouchEnabled(false);
+                graficaGeneral.getDescription().setText("");
+            }
+            else{
+                Toast.makeText(AsistenciaIndividualProfesor.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
+
+    public String nombreMes(int mes){
+        String nombreMes;
+        switch (mes) {
+            case 1:  nombreMes = "Enero";
+                break;
+            case 2:  nombreMes = "Febrero";
+                break;
+            case 3:  nombreMes = "Marzo";
+                break;
+            case 4:  nombreMes = "Abril";
+                break;
+            case 5:  nombreMes = "Mayo";
+                break;
+            case 6:  nombreMes = "Junio";
+                break;
+            case 7:  nombreMes = "Julio";
+                break;
+            case 8:  nombreMes = "Agosto";
+                break;
+            case 9:  nombreMes = "Septiembre";
+                break;
+            case 10: nombreMes = "Octubre";
+                break;
+            case 11: nombreMes = "Noviembre";
+                break;
+            case 12: nombreMes = "Diciembre";
+                break;
+            default: nombreMes = "Mes invalido";
+                break;
+        }
+        return nombreMes;
     }
 }
