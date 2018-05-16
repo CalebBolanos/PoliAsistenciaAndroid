15b1c5b3c463af3f511c80874e532007abd84f9a
Alexis Olvera <alexis.ol.me@gmail.com>
2018-05-15 20:03:20 -0500

    No sirve,pero ahorita se arregla :v

--- app/src/main/java/edu/cecyt9/ipn/poliasistenciaandroid/Configuracion.java
+++ app/src/main/java/edu/cecyt9/ipn/poliasistenciaandroid/Configuracion.java
@@ -2,12 +2,16 @@ package edu.cecyt9.ipn.poliasistenciaandroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -26,6 +30,15 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -34,6 +47,8 @@ import edu.cecyt9.ipn.poliasistenciaandroid.jefeAcademia.InicioJefe;
 import edu.cecyt9.ipn.poliasistenciaandroid.prefecto.InicioPrefecto;
 import edu.cecyt9.ipn.poliasistenciaandroid.profesor.InicioProfesor;
 
+import static edu.cecyt9.ipn.poliasistenciaandroid.InicioSesion.IP;
+import static edu.cecyt9.ipn.poliasistenciaandroid.InicioSesion.PUERTO;
 import static edu.cecyt9.ipn.poliasistenciaandroid.Sesion.ALUMNO;
 import static edu.cecyt9.ipn.poliasistenciaandroid.Sesion.JEFE_ACADEMIA;
 import static edu.cecyt9.ipn.poliasistenciaandroid.Sesion.PREFECTO;
@@ -46,7 +61,8 @@ public class Configuracion extends AppCompatActivity {
     TextView nombre, boleta, nacimiento;
     CircleImageView imagen;
     LinearLayout datosGenerales;
-
+    String resultado, nombreArchivo;
+    Bitmap bitMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,7 +81,7 @@ public class Configuracion extends AppCompatActivity {
         toolbar.setTitleTextColor((Color.parseColor("#ffffff")));
         setSupportActionBar(toolbar);
 
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Configuración");
             final Drawable flechaAtras = getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp);
             flechaAtras.setColorFilter(getResources().getColor(R.color.blanco), PorterDuff.Mode.SRC_ATOP);
@@ -75,11 +91,11 @@ public class Configuracion extends AppCompatActivity {
         }
 
         nombre = findViewById(R.id.textView2);
-        String nombreString = sesion.getNombre() +" "+sesion.getPaterno()+" "+sesion.getMaterno();
+        String nombreString = sesion.getNombre() + " " + sesion.getPaterno() + " " + sesion.getMaterno();
         nombre.setText(nombreString);
         imagen = findViewById(R.id.imageView);
         datosGenerales = findViewById(R.id.linear_datos);
-        Picasso.with(this).load("http://"+InicioSesion.IP+":"+InicioSesion.PUERTO+"/poliAsistenciaWeb/"+sesion.getUrlImagen()).into(imagen);
+        Picasso.with(this).load("http://" + InicioSesion.IP + ":" + InicioSesion.PUERTO + "/poliAsistenciaWeb/" + sesion.getUrlImagen()).into(imagen);
         boleta = findViewById(R.id.boletaDatos);
         boleta.setText(sesion.getNum());
         nacimiento = findViewById(R.id.nacimiento);
@@ -93,7 +109,7 @@ public class Configuracion extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String seleccionado = adapterView.getItemAtPosition(i).toString();
-                switch (seleccionado){
+                switch (seleccionado) {
                     case "Cambiar correo electrónico":
                         Intent cambioCorreo = new Intent(Configuracion.this, CambiarCorreo.class);
                         startActivity(cambioCorreo);
@@ -121,25 +137,8 @@ public class Configuracion extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK){
-            switch (requestCode){
-                case 1:
-                    if(data == null){
-
-                    }
-
-                    //InputStream input = getContex
-                    break;
-            }
-        }
-
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
@@ -150,7 +149,7 @@ public class Configuracion extends AppCompatActivity {
                 pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
-                        switch (menuItem.getItemId()){
+                        switch (menuItem.getItemId()) {
                             case R.id.tomar_foto:
                                 Intent camara = new Intent("android.media.action.IMAGE_CAPTURE");
                                 startActivity(camara);
@@ -165,7 +164,7 @@ public class Configuracion extends AppCompatActivity {
                     }
                 });
                 pop.show();
-            break;
+                break;
         }
 
 
@@ -197,6 +196,81 @@ public class Configuracion extends AppCompatActivity {
         params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
         listView.setLayoutParams(params);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    bitMap = (Bitmap) imageReturnedIntent.getExtras().get("data");
+                    nombreArchivo = imageReturnedIntent.getDataString();
+                    cambiarimagen cambImg = new cambiarimagen();
+                    cambImg.execute();
+                    imagen.setImageURI(selectedImage);
+                }
 
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imagen.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+    public class cambiarimagen extends AsyncTask<String, String, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            // Metodo que queremos ejecutar en el servicio web
+            String Metodo = "guradaImagenAndroid";
+// Namespace definido en el servicio web
+            String namespace = "http://servicios/";
+// namespace + metodo
+            String accionSoap = "hhtp://servicios/guradaImagenAndroid";
+// Fichero de definicion del servcio web
+            String url = "http://"+IP+":"+PUERTO+"/serviciosWebPoliAsistencia/usuario?WSDL";
+            Sesion ses = new Sesion(getApplicationContext());
+            String bol = ses.getNum();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitMap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+            byte[] bitmapdata = bos.toByteArray();
+            JSONObject jeiSon = new JSONObject();
+            try {
+                jeiSon.put("numero", bol);
+                jeiSon.put("img", bitmapdata);
+                jeiSon.put("nombreArchivo", nombreArchivo);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            SoapObject request = new SoapObject(namespace, Metodo);
+            request.addProperty("StringDeJSON", jeiSon);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(request);
+
+            envelope.dotNet = false;
+
+            HttpTransportSE ht = new HttpTransportSE(url);
+            ht.debug = true;
+
+            try {
+                ht.call(accionSoap, envelope);
+                SoapPrimitive responce = (SoapPrimitive) envelope.getResponse();
+                resultado = responce.toString();
+            } catch (Exception xd) {
+                Log.println(Log.ERROR, "Error: ", xd.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(resultado.equalsIgnoreCase("Se subio correctamente")){
+
+            }
+        }
+    }
 
 }
